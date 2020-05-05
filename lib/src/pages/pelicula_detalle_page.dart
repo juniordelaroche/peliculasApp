da6624938f7adba9ddde0cb0d37f4654435d4e96
Junior De La Roche <juniordelaroche@gmail.com>
2020-05-04 22:14:54 -0500

    Modificada la vista detalle, agregando titulo descripcion

--- lib/src/pages/pelicula_detalle_page.dart
+++ lib/src/pages/pelicula_detalle_page.dart
@@ -8,7 +8,16 @@ class PeliculaDetallePage extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
          slivers: <Widget>[
-           _crearAppBar(pelicula)
+           _crearAppBar(pelicula),
+           SliverList(
+             delegate: SliverChildListDelegate(
+              [
+                SizedBox(height: 10.0),
+                _posterTitulo(pelicula, context),
+                _descripcion(pelicula),
+              ]
+             )
+          )
          ],
       ),
     );
@@ -36,6 +45,49 @@ class PeliculaDetallePage extends StatelessWidget {
           fadeInDuration: Duration(milliseconds: 150),
           fit: BoxFit.cover
         ),
+      ),
+    );
+  }
+
+  Widget _posterTitulo(Pelicula pelicula, BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image(
+              image: NetworkImage(pelicula.getPosterImg()),
+              height: 150.0,
+            ),
+          ),
+          SizedBox(width: 15.0),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(pelicula.title, style: Theme.of(context).textTheme.title, overflow: TextOverflow.ellipsis),
+                Text(pelicula.originalTitle, style: Theme.of(context).textTheme.subhead, overflow: TextOverflow.ellipsis),
+                Row(
+                  children: <Widget>[
+                    Icon(Icons.star_border),
+                    Text(pelicula.voteAverage.toString(), style: Theme.of(context).textTheme.subhead,)
+                  ],
+                )
+              ]
+            )
+          )
+        ]
+      )
+    );
+  }
+
+  Widget _descripcion(Pelicula pelicula) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+      child: Text(
+        pelicula.overview,
+        textAlign: TextAlign.justify,
       ),
     );
   }
